319a799c7117cba04f71ddd8b0f14643e4e436b1
Bintang <itzbintangyosua@gmail.com>
2024-06-21 14:08:45 +0700

    production?

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const fs = require("fs");
 require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  Embed,
+  PresenceUpdateStatus,
+  ActivityType,
+} = require("discord.js");
 const { displayChatTokenCount, streamToStdout } = require("./utils/common.js");
+const { EmbedBuilder } = require("discord.js");
 
-const allowedChannelIds = ["690654777608110161", "1253520252600455329"];
+const allowedChannelIds = [
+  "690654777608110161",
+  "1253520252600455329",
+  "1253590960575877120",
+];
 
 // Get your API key from https://makersuite.google.com/app/apikey
 // Access your API key as an environment variable
@@ -15,12 +26,6 @@ const chatInstances = {};
 
 async function run() {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
-  const chat = model.startChat({
-    history: [],
-    generationConfig: {
-      maxOutputTokens: 100,
-    },
-  });
 
   const client = new Client({
     intents: [
@@ -32,6 +37,11 @@ async function run() {
   });
 
   client.once("ready", () => {
+    client.user.setStatus(PresenceUpdateStatus.DoNotDisturb);
+    client.user.setActivity({
+      name: "Chatting with You",
+      type: ActivityType.Playing,
+    });
     console.log(`Logged in as ${client.user.tag}!`);
   });
 
@@ -53,13 +63,24 @@ async function run() {
       const chat = chatInstances[message.channel.id];
 
       const result = await chat.sendMessageStream(message.content);
+
       displayChatTokenCount(model, chat, message.content);
       await streamToStdout(result.stream);
 
+      const embed = new EmbedBuilder()
+        .setDescription((await result.response).text())
+        .setColor(0x845a5e)
+        .setFooter({
+          iconURL:
+            "https://cdn.discordapp.com/avatars/1138809326572679318/2de4f74ca51ac4642550fc7195e8d9b4.png",
+          text: "SPEAKABLE YOUTH",
+        });
+
       // console.log((await result.response).text());
-      await message.channel.send((await result.response).text());
+      await message.channel.send({ embeds: [embed] });
     } catch (error) {
       console.error("Error generating response:", error);
+      console.log(error.rawError.errors);
     }
   });
 
